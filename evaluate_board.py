0cba7299352fbb844f86d9a57fa9165e34340595
lordp <lordpizza37@gmail.com>
2025-04-25 07:36:13 +0200

    bug fixes

--- evaluate_board.py
+++ evaluate_board.py
@@ -16,25 +16,86 @@ class PositionalConstants:
 
 class ChessEvaluator:
     def __init__(self):
-        # Erhöhte Materialwerte
-        self.piece_values: Dict[str, float] = {
-            'P': 2,    # Bauer
-            'N': 6,    # Springer
-            'B': 6.4,  # Läufer
-            'R': 10,   # Turm
-            'Q': 18,   # Dame
-            'K': 0,    # König
-            'p': -2,
-            'n': -6,
-            'b': -6.4,
-            'r': -10,
-            'q': -18,
-            'k': 0
+        # Grundlegende Materialwerte
+        self.piece_values = {
+            'P': 100,    # Bauer
+            'N': 320,    # Springer
+            'B': 330,    # Läufer
+            'R': 500,    # Turm
+            'Q': 900,    # Dame
+            'K': 20000,  # König
+            'p': -100,
+            'n': -320,
+            'b': -330,
+            'r': -500,
+            'q': -900,
+            'k': -20000
         }
-        self.constants = PositionalConstants()
+        
+        # Positionelle Boni (maximal 10% des Figurenwerts)
+        self.positional_bonus = {
+            chess.PAWN: 10,     # 10% von 100
+            chess.KNIGHT: 32,   # 10% von 320
+            chess.BISHOP: 33,   # 10% von 330
+            chess.ROOK: 50,     # 10% von 500
+            chess.QUEEN: 90,    # 10% von 900
+            chess.KING: 20      # Kleiner Bonus für König
+        }
+
+    def evaluate_board(self, board: chess.Board) -> float:
+        if board.is_checkmate():
+            return -20000 if board.turn else 20000
+            
+        if board.is_stalemate() or board.is_insufficient_material():
+            return 0.0
+            
+        score = self.evaluate_material(board)
+        score += self.evaluate_position(board)
+        
+        return score
+
+    def evaluate_material(self, board: chess.Board) -> float:
+        """Bewertet nur das Material auf dem Brett."""
+        return sum(self.piece_values[p.symbol()] for p in board.piece_map().values())
+
+    def evaluate_position(self, board: chess.Board) -> float:
+        """Bewertet die Position der Figuren."""
+        score = 0.0
+        
+        for square in chess.SQUARES:
+            piece = board.piece_at(square)
+            if not piece:
+                continue
+                
+            bonus = 0.0
+            rank = chess.square_rank(square)
+            file = chess.square_file(square)
+            
+            if board.fullmove_number <= 10:  # Eröffnungsphase
+                if piece.piece_type == chess.PAWN:
+                    # Zentrumsbonus für Bauern
+                    if 2 <= file <= 5 and 3 <= rank <= 4:
+                        bonus = self.positional_bonus[chess.PAWN] * 0.5
+                        
+                elif piece.piece_type in [chess.KNIGHT, chess.BISHOP]:
+                    # Entwicklungsbonus
+                    if (piece.color == chess.WHITE and rank > 0) or \
+                       (piece.color == chess.BLACK and rank < 7):
+                        bonus = self.positional_bonus[piece.piece_type] * 0.3
+                        
+                elif piece.piece_type == chess.QUEEN:
+                    # Bestrafung für frühe Damenzüge
+                    if (piece.color == chess.WHITE and square != chess.D1) or \
+                       (piece.color == chess.BLACK and square != chess.D8):
+                        bonus = -self.positional_bonus[chess.QUEEN] * 0.2
+
+            # Addiere/Subtrahiere den Bonus je nach Farbe
+            score += bonus if piece.color else -bonus
+            
+        return score
 
     def evaluate_material_change(self, board: chess.Board, move: chess.Move) -> float:
-        """Bewertet Materialänderungen mit zusätzlicher Bestrafung für Verluste."""
+        """Bewertet Materialänderungen bei einem Zug."""
         if not board.is_capture(move):
             return 0.0
 
@@ -49,39 +110,6 @@ class ChessEvaluator:
 
         # Wenn der Wert der geschlagenen Figur kleiner ist
         if captured_value < moving_value:
-            # Zusätzliche Bestrafung für schlechte Tausche
-            return -((moving_value - captured_value) * 1.5)
+            return -(moving_value - captured_value) * 0.1  # Reduzierte Bestrafung
         
-        return captured_value - moving_value
-
-    def evaluate_piece_position(self, piece: chess.Piece, square: int) -> float:
-        """Bewertet die Position einer Figur auf dem Brett."""
-        if piece.piece_type == chess.PAWN:
-            if chess.square_rank(square) in [3, 4]:
-                return self.constants.CENTRAL_PAWN_BONUS
-            return 0.0
-
-        if piece.piece_type == chess.KNIGHT:
-            if chess.square_rank(square) in [3, 4]:
-                return self.constants.CENTRAL_KNIGHT_BONUS
-            return 0.0
-
-        # Weitere Bewertungen für andere Figuren können hier hinzugefügt werden
-        return 0.0
-
-    def evaluate_board(self, board: chess.Board) -> float:
-        """Bewertet das Schachbrett und gibt eine Bewertung zurück."""
-        if board.is_checkmate():
-            return self.constants.CHECKMATE_BONUS
-
-        if board.is_stalemate():
-            return 0.0
-
-        score = 0.0
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            if piece:
-                score += self.piece_values[piece.symbol()]
-                score += self.evaluate_piece_position(piece, square)
-
-        return score
+        return (captured_value - moving_value) * 0.1  # Reduzierter Bonus
